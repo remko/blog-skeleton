e87c236d2b3beb498bed8541c1f2ecc47b9a229e
Remko Tronçon <git@el-tramo.be>
2013-02-24 15:39:03 +0100

    Redirect git links to GitHub.

---
+++ lib/git_link_filter.rb
@@ -0,0 +1,12 @@
+# A filter to redirect all /git urls to GitHub
+class GitLinkFilter < Nanoc::Filter
+	identifier :git_link_filter
+
+	def run(content, params = {})
+		url_prefix = "https://github.com/#{@config[:github_id]}"
+		content = content.gsub(/href="\/git\/([^\/]+)\/tree/, "href=\"#{url_prefix}/\\1/blob/master")
+		content = content.gsub(/href="\/git\/([^\/]+)\/snapshot\/(.*)-master\.zip/, "href=\"#{url_prefix}/\\1/archive/master.zip")
+		content = content.gsub(/href="\/git\/([^\/]+)\/?"/, "href=\"#{url_prefix}/\\1/\"")
+		content = content.gsub(/href="\/git\/?"/, "href=\"#{url_prefix}/\"")
+	end
+end
