ba8eb666728f9830655e68852f15ab85ef57b327
Remko Tronçon <git@el-tramo.be>
2013-05-30 20:58:25 +0200

    Added plain text link support to git link filter.

--- lib/git_link_filter.rb
+++ lib/git_link_filter.rb
@@ -4,8 +4,11 @@ class GitLinkFilter < Nanoc::Filter
 
 	def run(content, params = {})
 		url_prefix = "https://github.com/#{@config[:github_id]}"
+		raw_url_prefix = "https://raw.github.com/#{@config[:github_id]}"
+
 		content = content.gsub(/href="\/git\/([^\/]+)\/tree/, "href=\"#{url_prefix}/\\1/blob/master")
 		content = content.gsub(/href="\/git\/([^\/]+)\/snapshot\/(.*)-master\.zip/, "href=\"#{url_prefix}/\\1/archive/master.zip")
+		content = content.gsub(/href="\/git\/([^\/]+)\/plain\/([^\/]+)\/?"/, "href=\"#{raw_url_prefix}/\\1/master/\\2\"")
 		content = content.gsub(/href="\/git\/([^\/]+)\/?"/, "href=\"#{url_prefix}/\\1/\"")
 		content = content.gsub(/href="\/git\/?"/, "href=\"#{url_prefix}/\"")
 	end
