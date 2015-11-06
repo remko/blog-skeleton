c68d32d9ca2698cf2208c30178082c31431eb0f6
Remko Tronçon <git@el-tramo.be>
2015-11-06 20:29:47 +0100

    Disable search until data is ready.

--- content/js/search.js
+++ content/js/search.js
@@ -3,5 +3,6 @@
 (function (window) {
 	<% require 'json' %>
 	window.searchData = <%= JSON.generate(search_index(items.select { |i| is_content?(i) })) %>;
-	document.querySelector(".search-form").style.display = "block";
+	document.querySelector("#search-form .search-field").disabled = false;
+	document.querySelector("#search-form .search-submit").disabled = false;
 })(window);
